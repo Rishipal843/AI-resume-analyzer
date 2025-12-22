78a989a810c18eedc888bd14e8507d4cc02edc5c
Rishipal singh <rishi191816@gmail.com>
2025-12-22 21:16:46 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -7,7 +7,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://ai-resume-analyzer-pi-opal.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
